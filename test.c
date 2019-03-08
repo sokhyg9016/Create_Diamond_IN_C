f6f562e638f3578c3f41cd6ae188a8d6d4834de7
sokhyg9016 <sokhyg9016@naver.com>
2019-03-08 15:19:05 +0900

    Diamond Implement C Code

---
+++ test.c
@@ -0,0 +1,63 @@
+#pragma warning(disable: 4996)
+#include<stdio.h>
+#include<stdlib.h>
+#define abs(x) ((x > 0)?(x):(-x))
+
+typedef unsigned char binary;
+
+enum
+{
+	BIT_TO_ZERO = 0,
+	BIT_TO_ONE
+};
+
+binary ResetBit(binary dest_data, binary bit_num)
+{
+	return dest_data & ~(0x01 << bit_num);
+}
+
+int main()
+{
+	/*
+
+	  *  ******  *
+	 ***  ****  ***
+	*****  **  *****
+   *******    *******
+  *********  *********
+  *********  *********	
+   *******    *******
+    *****  **  *****
+	 ***  ****  ***
+	  *  ******  *
+
+
+	*/
+
+	int i, j;
+
+	int height = 5;
+	
+	fputs("INPUT HEIGHT: ", stdout);
+	scanf_s("%d", &height);
+
+	for (i = -height; i < height; i++)
+	{
+		for (j = 0; j < abs(i); j++)
+		{
+			printf("бр");
+		}
+		for (j = 0; j < (height - abs(i)) * 2 - 1; j++)
+		{
+			printf("бс");
+		}
+		puts("");
+	}
+
+
+	binary lamp_state = ResetBit(0xff, 2);
+
+	//printf("lamp_state= %#X, lamp_state = %d\n", lamp_state, lamp_state);
+	
+	return 0;
+}

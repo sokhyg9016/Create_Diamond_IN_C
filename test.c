7206a744fcb953edbb8c12368f221bbf108cc2b1
sokhyg9016 <sokhyg9016@naver.com>
2019-03-08 15:24:38 +0900

    Change UTF-8 Encoding

--- test.c
+++ test.c
@@ -1,39 +1,9 @@
-﻿#pragma warning(disable: 4996)
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<stdlib.h>
 #define abs(x) ((x > 0)?(x):(-x))
 
-typedef unsigned char binary;
-
-enum
-{
-	BIT_TO_ZERO = 0,
-	BIT_TO_ONE
-};
-
-binary ResetBit(binary dest_data, binary bit_num)
-{
-	return dest_data & ~(0x01 << bit_num);
-}
-
 int main()
 {
-	/*
-
-	  *  ******  *
-	 ***  ****  ***
-	*****  **  *****
-   *******    *******
-  *********  *********
-  *********  *********	
-   *******    *******
-    *****  **  *****
-	 ***  ****  ***
-	  *  ******  *
-
-
-	*/
-
 	int i, j;
 
 	int height = 5;
@@ -53,11 +23,6 @@ int main()
 		}
 		puts("");
 	}
-
-
-	binary lamp_state = ResetBit(0xff, 2);
-
-	//printf("lamp_state= %#X, lamp_state = %d\n", lamp_state, lamp_state);
 	
 	return 0;
 }
